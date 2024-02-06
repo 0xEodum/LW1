44ae6fec476f5bc9665ecf34fa79d6471503d653
komarov-a-kubsu <neuroxhk@gmail.com>
2024-02-06 22:58:42 +0300

    EX2 basic version second (9th ex) function

--- EX2_Basic.py
+++ EX2_Basic.py
@@ -2,4 +2,16 @@ def count_chars(data):
     rus_letters = sum(1 for ch in data if ch >= "А" and ch <= "я")
     return rus_letters
 
+def is_palindrome(data):
+    latin_letters = "abcdefghijklmnopqrstuvwxyz"
+    filtered_text = ""
+
+    for char in data:
+        if char in latin_letters:
+            filtered_text += char
+
+    filtered_text = filtered_text.lower()
+
+    return filtered_text == filtered_text[::-1]
+
 if __name__ == '__main__':
